0a088e0cb907d2ec93cf19af4947d2d95eb23b57
NDS3K <160611407+NDS3K@users.noreply.github.com>
2025-06-14 23:39:30 +0700

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,12 @@
 #include "Controller.h"
+// You should not change this function, if only you use different board (If you really want to change, you can change the pins in Configs.h)
 void setup(){
-    initialize();
+    initialize(); 
     setupPS2controller();
 }
+
 void loop()
 {   
-    ps2x.read_gamepad(false, false);
+    ps2x.read_gamepad(false, false); // Read the gamepad state, It's compulsory if you want to use the PS2 controller
     controller();
     delay(50);
-}
