ba5e709014ab43a0e5642fb75fbda71a661a95ec
NDS3K <160611407+NDS3K@users.noreply.github.com>
2025-06-14 23:38:35 +0700

    Update Configs.h

--- src/Configs.h
+++ src/Configs.h
@@ -6,21 +6,29 @@
 
 #define pin1_dc1 8 
 #define pin2_dc1 9 
+
 #define pin1_dc2 10
 #define pin2_dc2 11
+
 #define pin1_dc3 12
 #define pin2_dc3 13
+
 #define pin1_dc4 14
 #define pin2_dc4 15
 
 //Servo
 #define servo_1 2
 #define servo_2 3
-#define servo_3 3
-#define servo_4 4
+#define servo_3 4
+#define servo_4 5
+//PS2 
+#define PS2_DAT 12 // MISO
+#define PS2_CMD 13 // MOSI
+#define PS2_SEL 15 // SS
+#define PS2_CLK 14 // SLK
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver();
-
+PS2X ps2x;
 void initialize()
 {
     Wire.begin();
